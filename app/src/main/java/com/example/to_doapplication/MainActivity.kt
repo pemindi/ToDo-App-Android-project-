7532a8700c9a1295b509d36b85b615b4da1e012c
pemindi <pemindiw@gmail.com>
2024-05-13 12:18:00 +0530

    project added

--- app/src/main/java/com/example/to_doapplication/MainActivity.kt
+++ app/src/main/java/com/example/to_doapplication/MainActivity.kt
@@ -10,12 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Delayed navigation to MainActivity2
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-            finish() // Optional, to close MainActivity once MainActivity2 is started
-        }, 3000) // 3000 milliseconds delay
     }
 }
